4195e3ae2caf40912918479857ccf03ab8f26b0a
Eric Camellini <eric.camellini@gmail.com>
2022-01-21 13:17:49 +0100

    Validating getTrips API call response using io-ts

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,8 @@
+import { Trip } from "./models";
+import * as t from "io-ts";
+import { PathReporter } from "io-ts/PathReporter";
+import { either } from "fp-ts";
+
 const baseUrl = process.env.REACT_APP_API_URL;
 const apiKey = process.env.REACT_APP_API_KEY;
 
@@ -10,4 +15,15 @@ const get = (path: string) =>
     .then((res) => res.json())
     .then((res) => res.records.map((r: { fields: unknown[] }) => r.fields));
 
-export const getTrips = async () => await get("/Trips");
+export const getTrips = async (): Promise<Trip[]> => {
+  const response = await get("/Trips");
+  return decodeOrThrow(t.array(Trip), response);
+};
+
+function decodeOrThrow<A, O>(type: t.Type<A, O>, value: unknown): A {
+  const decoded = type.decode(value);
+  if (either.isLeft(decoded)) {
+    throw new Error(PathReporter.report(decoded).join("\n"));
+  }
+  return decoded.right;
+}
