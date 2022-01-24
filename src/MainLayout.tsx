07d94a4c07bf543ee4b8bc3665ad19f14bce5852
Eric Camellini <eric.camellini@gmail.com>
2022-01-24 13:27:17 +0100

    Adding language change dropdown

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -3,10 +3,20 @@ import * as styles from "./MainLayout.css";
 import { Trips } from "./Trips";
 
 export function MainLayout() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
   return (
     <div className={styles.mainLayout}>
-      <header className={styles.header}>{t("MainLayout.Trips")}</header>
+      <header className={styles.header}>
+        {t("MainLayout.Trips")}
+        <select
+          value={i18n.language}
+          onChange={(e) => i18n.changeLanguage(e.currentTarget.value)}
+        >
+          <option value="en">{t("LanguageSelector.English")}</option>
+          <option value="it">{t("LanguageSelector.Italian")}</option>
+        </select>
+      </header>
       <main className={styles.main}>
         <Trips />
       </main>
