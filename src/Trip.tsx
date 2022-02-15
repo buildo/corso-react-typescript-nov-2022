a4d9f3152d40532e911c2e646009acd604568244
Gabriele Petronella <gabriele@buildo.io>
2022-02-15 10:58:15 +0100

    Add seatNumber

--- src/Trip.tsx
+++ src/Trip.tsx
@@ -4,9 +4,19 @@ import * as models from "./models";
 type Props = models.Trip;
 
 export function Trip(props: Props) {
+  const seatNumber = ((): string => {
+    switch (props.status) {
+      case "Booked":
+      case "Requested":
+        return "";
+      case "CheckedIn":
+        return `⋅ 💺 ${props.seatNumber}`;
+    }
+  })();
+
   return (
     <div className={`${styles.trip} ${styles.tripStatus[props.status]}`}>
-      <span>{`${props.origin} -> ${props.destination}`}</span>
+      <span>{`${props.origin} -> ${props.destination} ${seatNumber} `}</span>
       <span>{`${props.startDate.toDateString()} -> ${props.endDate.toDateString()}`}</span>
     </div>
   );
--- src/models.ts
+++ src/models.ts
@@ -1,22 +1,28 @@
 import * as t from "io-ts";
 import { DateFromISOString } from "io-ts-types/DateFromISOString";
 
-// Using t.keyof instead of t.union
-// See https://github.com/gcanti/io-ts/blob/master/index.md#union-of-string-literals
-// for an explanation
-export const TripStatus = t.keyof({
-  Requested: null,
-  Booked: null,
-  CheckedIn: null,
-});
+export const TripStatus = t.union([
+  t.type({
+    status: t.literal("Requested"),
+  }),
+  t.type({
+    status: t.literal("Booked"),
+  }),
+  t.type({
+    status: t.literal("CheckedIn"),
+    seatNumber: t.string,
+  }),
+]);
 export type TripStatus = t.TypeOf<typeof TripStatus>;
 
-export const Trip = t.type({
-  id: t.string,
-  origin: t.string,
-  destination: t.string,
-  startDate: DateFromISOString,
-  endDate: DateFromISOString,
-  status: TripStatus,
-});
+export const Trip = t.intersection([
+  t.type({
+    id: t.string,
+    origin: t.string,
+    destination: t.string,
+    startDate: DateFromISOString,
+    endDate: DateFromISOString,
+  }),
+  TripStatus,
+]);
 export type Trip = t.TypeOf<typeof Trip>;
