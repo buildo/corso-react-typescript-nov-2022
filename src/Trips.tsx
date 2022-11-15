d5a997040802f23d493373a4bfba0194fab59070
Francesco Cioria <firefelix@gmail.com>
2022-11-15 11:51:25 +0100

    Handle error and loading states with useQuery (v4)

--- src/Trips.tsx
+++ src/Trips.tsx
@@ -4,10 +4,16 @@ import * as styles from "./Trips.css";
 import { useQuery } from "@tanstack/react-query";
 
 export function Trips() {
-  const { data: trips } = useQuery({ queryKey: ["trips"], queryFn: getTrips });
+  const {
+    data: trips,
+    isError,
+    isLoading,
+  } = useQuery({ queryKey: ["trips"], queryFn: getTrips });
 
   return (
     <div className={styles.trips}>
+      {isError && <div>Error while fetching trips!!</div>}
+      {isLoading && <div>Loading...</div>}
       {trips?.map((trip) => (
         <Trip key={trip.id} {...trip} />
       ))}
