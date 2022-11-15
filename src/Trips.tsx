f13f236464e8534f1736573a1be6344b8b97f1ae
Francesco Cioria <firefelix@gmail.com>
2022-11-15 13:02:42 +0100

    useQuery with a switch on the status sum type (v4)

--- src/Trips.tsx
+++ src/Trips.tsx
@@ -2,21 +2,21 @@ import { getTrips } from "./api";
 import { Trip } from "./Trip";
 import * as styles from "./Trips.css";
 import { useQuery } from "@tanstack/react-query";
+import { ReactNode } from "react";
 
 export function Trips() {
-  const {
-    data: trips,
-    isError,
-    isLoading,
-  } = useQuery({ queryKey: ["trips"], queryFn: getTrips });
+  const query = useQuery({ queryKey: ["trips"], queryFn: getTrips });
 
-  return (
-    <div className={styles.trips}>
-      {isError && <div>Error while fetching trips!!</div>}
-      {isLoading && <div>Loading...</div>}
-      {trips?.map((trip) => (
-        <Trip key={trip.id} {...trip} />
-      ))}
-    </div>
-  );
+  const content = ((): Exclude<ReactNode, undefined> => {
+    switch (query.status) {
+      case "success":
+        return query.data.map((trip) => <Trip key={trip.id} {...trip} />);
+      case "error":
+        return "Error while fetching trips!!";
+      case "loading":
+        return "Loading...";
+    }
+  })();
+
+  return <div className={styles.trips}>{content}</div>;
 }
