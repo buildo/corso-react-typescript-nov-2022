ede1331227d51e903817fe7ba7dd4d73e7a758bb
Francesco Cioria <firefelix@gmail.com>
2022-11-15 11:51:00 +0100

    Use useQuery (v4) instead of useEffect

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { MainLayout } from "./MainLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 0 },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainLayout />
+    <QueryClientProvider client={queryClient}>
+      <MainLayout />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
