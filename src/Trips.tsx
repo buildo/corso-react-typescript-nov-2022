edjeezy/corso-react-typescript-feb-2022

Setting up localization with i18next

src/Trips.tsx
@@ -3,19 +3,21 @@ import { getTrips } from "./api";
 import { Trip } from "./Trip";
 import * as styles from "./Trips.css";
 import { matchQuery } from "./util/matchQuery";
+import { useTranslation } from "react-i18next";
 
 export function Trips() {
   const tripsQuery = useQuery("trips", getTrips);
+  const { t } = useTranslation();
 
   return (
     <div className={styles.trips}>
       {matchQuery(tripsQuery, {
         success: (trips, isFetching) =>
           isFetching
-            ? "Loading..."
+            ? t("TripDetails.QueryLoading")
             : trips.map((trip) => <Trip key={trip.id} {...trip} />),
-        error: () => "Error while fetching trips!!",
-        loading: () => "Loading...",
+        error: () => t("TripDetails.QueryError"),
+        loading: () => t("TripDetails.QueryLoading"),
       })}
     </div>
   );

buildo/corso-react-typescript-nov-2022

src/Trips.tsx
@@ -3,19 +3,21 @@ import { Trip } from "./Trip";
 import * as styles from "./Trips.css";
 import { useQuery } from "@tanstack/react-query";
 import { matchQuery } from "./utils/matchQuery";
+import { useTranslation } from "react-i18next";
 
 export function Trips() {
   const tripsQuery = useQuery({ queryKey: ["trips"], queryFn: getTrips });
+  const { t } = useTranslation();
 
   return (
     <div className={styles.trips}>
       {matchQuery(tripsQuery, {
         success: (trips, isFetching) =>
           isFetching
-            ? "Loading..."
+            ? t("Trips.QueryLoading")
             : trips.map((trip) => <Trip key={trip.id} {...trip} />),
-        error: () => "Error while fetching trips!!",
-        loading: () => "Loading...",
+        error: () => t("Trips.QueryError"),
+        loading: () => t("Trips.QueryLoading"),
       })}
     </div>
   );
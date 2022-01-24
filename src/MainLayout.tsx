2f1d61a66a54dbdaff104a50eb958b4ed0220d8b
Eric Camellini <eric.camellini@gmail.com>
2022-01-24 14:27:18 +0100

    Setting up localization with i18next

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -1,10 +1,12 @@
+import { useTranslation } from "react-i18next";
 import * as styles from "./MainLayout.css";
 import { Trips } from "./Trips";
 
 export function MainLayout() {
+  const { t } = useTranslation();
   return (
     <div className={styles.mainLayout}>
-      <header className={styles.header}>Welcome!</header>
+      <header className={styles.header}>{t("MainLayout.Trips")}</header>
       <main className={styles.main}>
         <Trips />
       </main>
--- src/Trips.tsx
+++ src/Trips.tsx
@@ -3,19 +3,21 @@ import { Trip } from "./Trip";
 import * as styles from "./Trips.css";
 import { useQuery } from "@tanstack/react-query";
 import { matchQuery } from "./utils/matchQuery";
+import { useTranslation } from "react-i18next";
 
 export function Trips() {
   const tripsQuery = useQuery({ queryKey: ["trips"], queryFn: getTrips });
+  const { t } = useTranslation();
 
   return (
     <div className={styles.trips}>
       {matchQuery(tripsQuery, {
         success: (trips, isFetching) =>
           isFetching
-            ? "Loading..."
+            ? t("Trips.QueryLoading")
             : trips.map((trip) => <Trip key={trip.id} {...trip} />),
-        error: () => "Error while fetching trips!!",
-        loading: () => "Loading...",
+        error: () => t("Trips.QueryError"),
+        loading: () => t("Trips.QueryLoading"),
       })}
     </div>
   );
